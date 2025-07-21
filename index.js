11a338e3501cf2266d29d175756305e89793c044
Iram Saba Khan <161953870+iramsk02@users.noreply.github.com>
2025-07-21 12:59:24 +0530

    third

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from CI/CD Node App running on EC2 with Docker!");
+  res.send("Hello from CI/CD Node App running on EC2 with Docker! made some changes ");
 });
 
 app.listen(PORT, () => {
